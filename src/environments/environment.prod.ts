bc025f877af9f2f84b62475f66dc49ba40b79e8b
Henri Kotkanen <henri.kotkanen@gmail.com>
2021-07-29 16:24:49 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiRootUrl: 'https://api.hel.fi/servicemap/v2/unit/',
+  apiRootUrl: 'https://api.hel.fi/servicemap/v2/unit',
   listApiParams: '?service_node=2006&only=location,name,street_address&page=1&page_size=1000',
   singleUnitApiParams: '?include=municipality'
 };
